5c1a07f6f191d1203bb7134b03780a223f4f836a
bradferd <bbailey921@gmail.com>
2020-05-27 14:25:03 -0400

    add tests for commentBox component

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,5 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default () => {
-    return <div>Comment Box</div>
+export default (props) => {
+    const [comment, setComment] = useState("");
+
+    return (
+        <form onSubmit={(e) => {
+            e.preventDefault();
+
+            // TODO - Call an action creator
+            // Save the comment
+            setComment('');
+        }}>
+            <h4>Add a Comment</h4>
+            <textarea onChange={e => {
+                setComment(e.target.value);
+            }} value={comment} />
+            <div>
+                <button>Submit Comment</button>
+            </div>
+        </form>
+    );
 }
