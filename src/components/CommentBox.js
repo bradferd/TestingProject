5815809698cbf588876f700ddb37fe141408efd5
bradferd <bbailey921@gmail.com>
2020-05-27 16:05:00 -0400

    add API call for integration test

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,25 +1,33 @@
-import React, { useState } from 'react';
-import { connect } from 'react-redux'
-import * as actions from 'actions';
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import * as actions from "actions";
 
-const CommentBox = ({ saveComment }) => {
-    const [comment, setComment] = useState("");
+const CommentBox = ({ saveComment, fetchComments }) => {
+  const [comment, setComment] = useState("");
 
-    return (
-        <form onSubmit={(e) => {
-            e.preventDefault();
-            saveComment(comment);
-            setComment('');
-        }}>
-            <h4>Add a Comment</h4>
-            <textarea onChange={e => {
-                setComment(e.target.value);
-            }} value={comment} />
-            <div>
-                <button>Submit Comment</button>
-            </div>
-        </form>
-    );
-}
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    saveComment(comment);
+    setComment("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h4>Add a Comment</h4>
+        <textarea
+          onChange={(e) => {
+            setComment(e.target.value);
+          }}
+          value={comment}
+        />
+        <div>
+          <button>Submit Comment</button>
+        </div>
+      </form>
+      <button onClick={fetchComments}>Fetch Comments</button>
+    </div>
+  );
+};
 
 export default connect(null, actions)(CommentBox);
